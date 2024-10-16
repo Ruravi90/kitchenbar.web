87292e22a225d94865a0615b17f46bf545ef8dfb
eruravia <edgarraa@algartech.com>
2024-10-16 12:18:41 -0600

    Auto-generated commit

---
+++ chunk-PJFNMHGI.js
@@ -0,0 +1,1 @@
+var a={production:!0,baseUrl:"https://ruravi90.github.io/kitchenbar.web/client/",apiBase:"https://kitchenbarapi-production.up.railway.app/api/",hubBase:"https://kitchenbarapi-production.up.railway.app/hubs/base",publicKey:"BGCRL5HzYcj1d9_XThOzy78j278TqvKRO_Qa08vR7BEDkTQt9x2yxXYQdKJbjRNyFV53JNq7pMx6naT8RwPbum4"};export{a};
--- chunk-UUS4ZWI4.js
+++
@@ -1,1 +0,0 @@
-var a={production:!0,baseUrl:"https://ruravi90.github.io/kitchenbar.web/client/",apiBase:"https://kitchenbarapi-production.up.railway.app/api/",hubBase:"https://kitchenbarapi-production.up.railway.app/hubs/base"};export{a};

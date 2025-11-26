386c61020fb097893a90ea17bd9aab30738da08a
Edgar Ruravi Aguilar Arrezola <eaguilar.arrezola@hotmail.com>
2025-11-26 10:12:43 -0600

    ruta api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     version: require('../../package.json').version,
     production: true,
-    baseUrl: 'http://kitchenbar.ruravi.com/client/',
-    apiBase: 'http://kitchenbar-api.ruravi.com/api/',
-    hubBase: 'http://kitchenbar-api.ruravi.com/hubs/base',
+    baseUrl: 'https://kitchenbar-web.vercel.app/client/',
+    apiBase: 'https://kitchenbarapi-production.up.railway.app/api/',
+    hubBase: 'https://kitchenbarapi-production.up.railway.app/hubs/base',
     publicKey:'BGCRL5HzYcj1d9_XThOzy78j278TqvKRO_Qa08vR7BEDkTQt9x2yxXYQdKJbjRNyFV53JNq7pMx6naT8RwPbum4'
 };
 
